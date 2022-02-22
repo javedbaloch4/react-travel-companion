44ab03397a8408151d0aba72694f690f71b2ac2a
Javed Baloch <mjavedahmed@live.com>
2022-02-22 20:06:56 +0500

    Placing Card

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const [places, setPlaces] = useState([])
   const [coordinates, setCoordinates] = useState()
-  const [bounds, setBounds] = useState(null)
+  const [bounds, setBounds] = useState({})
 
   // Get the current user location, as user open's the page
   useEffect(() => {
@@ -20,7 +20,7 @@ function App() {
   }, [])
 
   useEffect( () => {
-    getPlacesData(bounds.sw, bounds.ne)
+    getPlacesData(bounds.ne, bounds.sw)
       .then((data) => {
         console.log(data)
         setPlaces(data)
@@ -33,7 +33,7 @@ function App() {
       <Header />
       <Grid container spacing={3} style={{width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,22 +4,10 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails'
 
 import useStyles from './styles'
 
-const List = () => {
+const List = ({places}) => {
     const classes = useStyles()
     const [type, setType] = useState('restaurants')
     const [rating, setRating] = useState('All')
-
-    const places = [
-        { name: 'Cool Place' },
-        { name: 'Best Beer' },
-        { name: 'Best Starks' },
-        { name: 'Best Starks' },
-        { name: 'Best Starks' },
-        { name: 'Best Starks' },
-        { name: 'Best Starks' },
-        { name: 'Best Starks' },
-        { name: 'Best Starks' },
-    ]
 
     return (
         <div className={classes.container}>
